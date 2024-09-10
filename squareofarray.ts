fee901e8eaff8a73a420fcbdd411987a9b155950
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-09-10 22:43:11 +0200

    sqaure of sorted array

---
+++ squareofarray.ts
@@ -0,0 +1,28 @@
+function sortedSquares(nums:number[]) {
+ //  return nums.map((e)=>Math.pow(e,2)).sort((a,b)=>a-b); 
+   /**
+    * 
+    * sort function only convert into string and convert it lexographically
+    * 
+    * To ensure that numbers are sorted in numeric order, you need to provide a comparator function, like (a, b) => a - b. This comparator tells .sort() how to compare two numbers:
+
+   If a - b is negative, a comes before b.
+   If a - b is positive, b comes before a.
+   If a - b is zero, the two numbers are treated as equal.
+    * 
+    * 
+    */
+
+
+
+
+   let left=0;
+   let right=nums.length-1;
+
+   while(left<right){
+      
+   }
+
+};
+
+
