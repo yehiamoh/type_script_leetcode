17a79a4dd3cb9df6bdaeb6fcfbb4a738ac326b35
Yehia Mohamed <112318507+yehiamoh@users.noreply.github.com>
2024-10-15 05:29:39 +0200

    83. Remove Duplicates from Sorted List

---
+++ remove_dup.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+   let curr =head;
+ while(curr&&curr.next){
+  if(curr.val===curr.next.val){
+      curr.next=curr.next.next;
+  }else{
+  curr=curr.next;
+ }}
+ return head;
+};
